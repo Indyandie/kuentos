0f1bef853504893214cd23864027b15f921fcade
indyandie <1058837+Indyandie@users.noreply.github.com>
2023-12-23 17:47:55 -0600

    webcom: update classes

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,7 +2,7 @@
 class MyComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <button class="bg-custom-blue hover:bg-custom-hover-blue text-white font-bold py-2 px-custom-padding rounded font-custom-font">
+      <button class="btn-primary">
         Click me
       </button>
     `;

a2f69858c0a855fdb2c317490464189c14143ccc
indyandie <1058837+Indyandie@users.noreply.github.com>
2023-12-23 02:35:56 -0600

    feat(tailwind): add styles

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,6 +1,11 @@
+// src/components/MyComponent.js
 class MyComponent extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `<p class="text-blue-500">Hello, world!</p>`;
+    this.innerHTML = `
+      <button class="bg-custom-blue hover:bg-custom-hover-blue text-white font-bold py-2 px-custom-padding rounded font-custom-font">
+        Click me
+      </button>
+    `;
   }
 }
 customElements.define('my-component', MyComponent);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx,html}',
+    // Add other file paths or globs as needed
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-blue': '#4B9EDF',
+        'custom-hover-blue': '#357ABD',
+      },
+      fontFamily: {
+        'custom-font': ['Arial', 'sans-serif'],
+      },
+      spacing: {
+        'custom-padding': '1rem',
+      },
+      // Add other customizations here
+    },
   },
-  plugins: [],
-}
+  // ... (other configurations)
+};
+
 
